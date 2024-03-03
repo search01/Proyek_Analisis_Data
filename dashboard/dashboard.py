09fd9fd4b598faf22de4c57db260d192a792ad01
Rosalia Dina <99029735+search01@users.noreply.github.com>
2024-03-04 00:54:18 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,8 +1,8 @@
-import streamlit as st
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import streamlit as st
+from babel.numbers import format_currency
 
 # Fungsi untuk menemukan jam dengan jumlah penyewaan sepeda paling banyak
 def find_peak_rental_hour(df):
